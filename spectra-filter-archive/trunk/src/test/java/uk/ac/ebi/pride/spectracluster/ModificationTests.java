64c2d8269aa24d52d769634c4c22709385e554f9
Lordjoe2000@gmail.com <Lordjoe2000@gmail.com@332a39bd-9e54-5415-0f82-69358dc792e7>
2014-09-08 21:09:20 +0000

    Added support for modifications as spectral property modifications also using 1.02 of clustering

---
+++ spectra-filter-archive/trunk/src/test/java/uk/ac/ebi/pride/spectracluster/ModificationTests.java
@@ -0,0 +1,45 @@
+package uk.ac.ebi.pride.spectracluster;
+
+import uk.ac.ebi.pride.jmztab.model.*;
+import uk.ac.ebi.pride.spectracluster.filter.archive.*;
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * uk.ac.ebi.pride.spectracluster.ModificationTests
+ * User: Steve
+ * Date: 9/8/2014
+ */
+public class ModificationTests {
+    public static final ModificationTests[] EMPTY_ARRAY = {};
+
+
+
+    public static void main(String[] args) throws Exception {
+        File mzTab = new File(args[0]);
+        File projectInternalPath = new File(System.getProperty("user.dir"));
+        ArchiveSpectra spec = ArchiveProjectSpectraFilter.buildArchiveSpectra(mzTab, projectInternalPath);
+
+        MZTabFile mzTabFile = spec.getMzTabFile();
+        for (PSM p : mzTabFile.getPSMs()) {
+            SplitList<Modification> modifications = p.getModifications();
+            int size = modifications.size();
+            boolean empty = modifications.isEmpty();
+            if(!empty && size  > 0)   {
+
+                String s = modifications.toString();
+                for (Modification modification : modifications) {
+                    System.out.print(modification);
+                  }
+                System.out.println();
+            }
+            else {
+                System.out.println("No modifications");
+            }
+          }
+        List<File> mgfFiles = spec.getMgfFiles();
+
+
+    }
+}
