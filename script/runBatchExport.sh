68cf27d6e5c3f8d30de7192022f36b479282ca84
Rui Wang <harry.r.wang@gmail.com>
2014-09-15 13:55:45 +0000

    reverted changes on runBatchExport.sh

--- script/runBatchExport.sh
+++ script/runBatchExport.sh
@@ -3,8 +3,9 @@
 ##### OPTIONS
 OUTPUT_PATH=$1
 FILTER_XML=$2
+INPUT_ROOT_PATH=/nfs/pride/prod/archive
 JOB_FILE=$3
 
 while read p; do
-    ./runExport.sh ${OUTPUT_PATH} ${FILTER_XML} $p
+    ./runExport.sh ${OUTPUT_PATH} ${FILTER_XML} ${INPUT_ROOT_PATH}/$p
 done < ${JOB_FILE}
