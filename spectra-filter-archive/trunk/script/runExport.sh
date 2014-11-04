5278d688397395b93d98b974bf1cfdbffc5c77a8
harry.r.wang@gmail.com <harry.r.wang@gmail.com@332a39bd-9e54-5415-0f82-69358dc792e7>
2014-11-04 17:30:51 +0000

    updated shell script to log to file

--- spectra-filter-archive/trunk/script/runExport.sh
+++ spectra-filter-archive/trunk/script/runExport.sh
@@ -5,6 +5,10 @@ OUTPUT_PATH=$1
 FILTER_XML=$2
 INPUT_PATH=$3
 
+# Log file name
+LOG_FILE_NAME=$(basename ${INPUT_PATH})
+MEMORY_LIMIT=4000
+
 ##### RUN it on the production LSF cluster
 ## this is not queued in the PRIDE LSF submission group, this is submitted as regular job as it is independent of any other job
-bsub -M 4000 -R "rusage[mem=4000]" -q production-rh6 -u pride.eb.test@gmail.com -J PRIDE-CLUSTER-EXPORT java -Xmx4000m -jar spectra-filter-archive-1.0-SNAPSHOT.jar ${OUTPUT_PATH} ${FILTER_XML} ${INPUT_PATH}
+bsub -M ${MEMORY_LIMIT} -R "rusage[mem=${MEMORY_LIMIT}]" -q production-rh6 -o /dev/null -J PRIDE-CLUSTER-EXPORT ./runJava.sh ./log/${LOG_FILE_NAME}.log ${MEMORY_LIMIT}m -jar ${project.build.finalName}.jar ${OUTPUT_PATH} ${FILTER_XML} ${INPUT_PATH}
