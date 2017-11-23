9382514539603be4d1c7a4ce63cca0224c6fe73d
Yasset Perez-Riverol <ypriverol@gmail.com>
2017-11-23 23:01:06 +0000

    reanalysis pipeline working -- minor refinements

--- reanalysis-pipeline/src/main/java/uk/ac/eb/pride/cluster/reanalysis/util/CLIRunner.java
+++ reanalysis-pipeline/src/main/java/uk/ac/eb/pride/cluster/reanalysis/util/CLIRunner.java
@@ -5,10 +5,7 @@
  */
 package uk.ac.eb.pride.cluster.reanalysis.util;
 
-import org.apache.commons.cli.BasicParser;
-import org.apache.commons.cli.CommandLine;
-import org.apache.commons.cli.Options;
-import org.apache.commons.cli.ParseException;
+import org.apache.commons.cli.*;
 import org.apache.log4j.Logger;
 
 import java.io.File;
@@ -22,29 +19,14 @@ public class CLIRunner {
 
     private static final Logger LOGGER = Logger.getLogger(CLIRunner.class);
 
-    private static Options GetOptions(boolean fromFile) {
-        Options options = new Options();
-        options.addOption("help", false, "Help message on the usage of the commandline");
-        options.addOption("fasta", true, "The sequence database to be searched");
-        options.addOption("out", true, "The output folder for the results");
-        options.addOption("peptideshaker", false, "Indicating if PeptideShaker needs to be run");
-        if (fromFile) {
-            options.addOption("id_params", true, "The compomics Identification Parameters file");
-            options.addOption("spectrum_files", true, "The input spectrum files (MGF)");
-        } else {
-            options.addOption("assay", true, "The PRIDE assay identifier");
-        }
-        return options;
-    }
 
     public static void BuildIntegrationCommandLine(String[] args) {
-        boolean fromFile = !Arrays.asList(args).contains("-assay");
 
-        BasicParser parser = new BasicParser();
+        DefaultParser parser = new DefaultParser();
 
         //parse the command line
         try {
-            CommandLine line = parser.parse(GetOptions(fromFile), args);
+            CommandLine line = parser.parse(GetOptions(), args);
             String fasta = null;
             File outputFolder = null;
             boolean isValidCLI = true;
@@ -66,48 +48,43 @@ public class CLIRunner {
                 }
             }
 
-            if (!fromFile) {
-                String assay = null;
-                if (line.hasOption("assay")) {
-                    assay = line.getOptionValue("assay");
-                } else {
-                    LOGGER.error("Assay is a mandatory parameter in case files are not provided.");
-                    isValidCLI = false;
-                }
-//                if (isValidCLI) {
-//                    Integration.run(assay, fasta, outputFolder, runPeptideShaker);
-//                }
+            String spectrum_files = null;
+            String id_params = null;
+            if (line.hasOption("spectrum_files")) {
+                spectrum_files = line.getOptionValue("spectrum_files");
             } else {
-                String spectrum_files = null;
-                String id_params = null;
-                if (line.hasOption("spectrum_files")) {
-                    spectrum_files = line.getOptionValue("spectrum_files");
-                } else {
-                    LOGGER.error("spectrum_files is a mandatory parameter in case an assay identifier is not provided.");
-                    isValidCLI = false;
-                }
-                if (line.hasOption("id_params")) {
-                    id_params = line.getOptionValue("id_params");
-                } else {
-                    LOGGER.error("id_params is a mandatory parameter in case an assay identifier is not provided.");
-                    isValidCLI = false;
-                }
-                if (isValidCLI) {
-                    IntegrationFromFile.run(spectrum_files, id_params, fasta, outputFolder, runPeptideShaker);
-                }
+                LOGGER.error("spectrum_files is a mandatory parameter in case an assay identifier is not provided.");
+                isValidCLI = false;
             }
+            if (line.hasOption("id_params")) {
+                id_params = line.getOptionValue("id_params");
+            } else {
+                LOGGER.error("id_params is a mandatory parameter in case an assay identifier is not provided.");
+                isValidCLI = false;
+            }
+            if (isValidCLI) {
+                IntegrationFromFile.run(spectrum_files, id_params, fasta, outputFolder, runPeptideShaker);
+            }
+
         } catch (ParseException ex) {
             LOGGER.error(ex);
         }
     }
 
     public static void main(String[] args) {
-        /*     args = new String[]{
-            "-assay", "11954",
-            "-out", "C:/Users/compomics/.compomics/pladipus/search/results",
-            "-fasta", "C:/Users/compomics/Documents//out/human_proteome_tincr_crap_concatenated_target_decoy.fasta"};
-       //     "-peptideshaker"};*/
         BuildIntegrationCommandLine(args);
     }
+
+    private static Options GetOptions() {
+        Options options = new Options();
+        options.addOption("help", false, "Help message on the usage of the commandline");
+        options.addOption("fasta", true, "The sequence database to be searched");
+        options.addOption("out", true, "The output folder for the results");
+        options.addOption("peptideshaker", false, "Indicating if PeptideShaker needs to be run");
+        options.addOption("id_params", true, "The compomics Identification Parameters file");
+        options.addOption("spectrum_files", true, "The input spectrum files (MGF)");
+        return options;
+    }
+
 
 }
