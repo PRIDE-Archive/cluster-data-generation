c69173dc630375f7d9a85273b4cdff3a3a938eb3
Yasset Perez-Riverol <ypriverol@gmail.com>
2017-11-29 17:22:17 +0000

    assembly build

--- scripts/parameters/PeptideAtlasParametersLSF.sh
+++ scripts/parameters/PeptideAtlasParametersLSF.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
+LOG_FOLDER=/nfs/pride/prod/logs/cluster/
+
 ### This script take a PeptideAtlas path and compute the corresponding Parameters ###
 
 ##### PARAMETERS
-MEMORY_LIMIT=$1
+INPUT_PATH=$1
+MEMORY_LIMIT=$3
 OUTPUT_PATH=$2
 
 ##### SCRIPT VARIABLES
@@ -21,11 +24,11 @@ LOG_FILE_NAME=$(basename ${INPUT_PATH})
 
 ##### FUNCTIONS
 printUsage() {
-    echo "Description: Import of PRIDE Complete Submissions into PRIDE Cluster"
+    echo "LSF script to run estimate the parameters from PeptideAtlas submissions"
     echo ""
-    echo "Usage: ./runExport.sh <outputpath> <filter-xml> <inputpath>"
+    echo "Usage: ./PeptideAtlasParametersLSF.sh <input-path> <output-path> <memory-limit>"
 }
 
 ##### RUN it on the production LSF cluster
 ## this is not queued in the PRIDE LSF submission group, this is submitted as regular job as it is independent of any other job
-bsub -M ${MEMORY_LIMIT} -R "rusage[mem=${MEMORY_LIMIT}]" -q production-rh7 -g /pride_cluster_archive_import -u ${JOB_EMAIL} -J ${JOB_NAME}-${NOW}-${LOG_FILE_NAME} ./runJava.sh ./log/${LOG_FILE_NAME}-${NOW}.log ${MEMORY_LIMIT}m -jar ${project.build.finalName}.jar -cp uk.ac.ebi.pride.spectracluster.cli.ImportFromArchiveCLI -o ${OUTPUT_PATH} -c ${FILTER_XML} -i ${INPUT_PATH} -s
+bsub -M ${MEMORY_LIMIT} -R "rusage[mem=${MEMORY_LIMIT}]" -q production-rh7 -g /pride_cluster_archive_import -u ${JOB_EMAIL} -J ${JOB_NAME}-${NOW}-${LOG_FILE_NAME} ./runJava.sh $LOG_FOLDER/${LOG_FILE_NAME}-${NOW}.log ${MEMORY_LIMIT}m -jar ${project.build.finalName}.jar -cp uk.ac.ebi.pride.spectracluster.cli.ImportFromArchiveCLI -o ${OUTPUT_PATH} -c ${FILTER_XML} -i ${INPUT_PATH} -s
