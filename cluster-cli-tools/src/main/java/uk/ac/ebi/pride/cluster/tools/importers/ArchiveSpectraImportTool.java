a2fbb5d6e9e0be2b52695fba74683f2376055a26
Yasset Perez-Riverol <ypriverol@gmail.com>
2017-12-02 01:12:16 +0000

    remove scripts folder

--- cluster-cli-tools/src/main/java/uk/ac/ebi/pride/cluster/tools/importers/ArchiveSpectraImportTool.java
+++ cluster-cli-tools/src/main/java/uk/ac/ebi/pride/cluster/tools/importers/ArchiveSpectraImportTool.java
@@ -42,7 +42,7 @@ public class ArchiveSpectraImportTool implements ICommandTool {
         </properties>
      */
     public static void main(String[] args) throws IOException, ParseException {
-        ArchiveExtractParameterTool tool = new ArchiveExtractParameterTool();
+        ArchiveSpectraImportTool tool = new ArchiveSpectraImportTool();
         Options options = tool.initOptions();
         try {
             tool.runCommand(options, args);
