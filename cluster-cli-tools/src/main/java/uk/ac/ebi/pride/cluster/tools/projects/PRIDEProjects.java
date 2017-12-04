c770eb7a860cf3635d655aa9e5db31268d98f46a
Yasset Perez-Riverol <ypriverol@gmail.com>
2017-12-04 17:46:16 +0000

    minor changes in the spectra import from Archive

---
+++ cluster-cli-tools/src/main/java/uk/ac/ebi/pride/cluster/tools/projects/PRIDEProjects.java
@@ -0,0 +1,93 @@
+package uk.ac.ebi.pride.cluster.tools.projects;
+
+import org.apache.log4j.Logger;
+import uk.ac.ebi.pride.cluster.tools.importers.ArchiveSpectraImportTool;
+
+import java.io.*;
+import java.net.URISyntaxException;
+import java.sql.*;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+import java.util.Properties;
+
+/**
+ * This code is licensed under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * ==Overview==
+ * <p>
+ * This class perform simple queries into oracle database an retrieve information about projects
+ * like:
+ *  - Path of public projects
+ *
+ * <p>
+ * Created by ypriverol (ypriverol@gmail.com) on 04/12/2017.
+ */
+public class PRIDEProjects {
+
+    //Logger file
+    private static final Logger LOGGER = Logger.getLogger(PRIDEProjects.class);
+
+    //Connection pull
+    private final Connection conn = generateConnection();
+
+    private Connection generateConnection() {
+        Properties props = new Properties();
+
+        InputStream input = null;
+        try {
+            input = new FileInputStream(new File(PRIDEProjects.class.getClassLoader().getResource("pride_archive.properties").toURI()));
+            props.load(input);
+            Connection con = DriverManager.getConnection(props.getProperty("pride-machine"),
+                    props.getProperty("pride-user"),
+                    props.getProperty("pride-pwd"));
+            LOGGER.info(con.toString());
+            return con;
+
+        } catch (URISyntaxException | IOException | SQLException e) {
+               e.printStackTrace();
+        }
+        return null;
+    }
+
+
+    public List<String> getPublicProjectURL(){
+       String query = "select accession, publication_date from project where (submission_type='PRIDE' or submission_type='COMPLETE') and is_public = 1";
+       List<String> listProjects = new ArrayList<>();
+       try (Statement stmt = conn.createStatement()) {
+           ResultSet rs = stmt.executeQuery(query);
+           while (rs.next()) {
+                String idProject = rs.getString("ACCESSION");
+                Date date = rs.getDate("PUBLICATION_DATE");
+                LOGGER.info(rs.toString());
+                listProjects.add(buildPath(idProject, date));
+            }
+        } catch (SQLException e) {
+            e.getMessage();
+        }
+        return listProjects;
+
+    }
+
+    /**
+     * Build PRIDE project path
+     * @param idProject id of the project
+     * @param date date of the project publication
+     * @return path of the project
+     */
+    private String buildPath(String idProject, Date date) {
+
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("YYYY");
+        String year = simpleDateFormat.format(date).toUpperCase();
+
+        simpleDateFormat = new SimpleDateFormat("MM");
+        String month = simpleDateFormat.format(date).toUpperCase();
+
+        return String.valueOf(year) + "/" + String.valueOf(month) + "/" + idProject + "/";
+    }
+}
